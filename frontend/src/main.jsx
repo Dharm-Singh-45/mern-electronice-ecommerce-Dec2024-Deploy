2918018fa89c582709a54e61f0b34a8c3f076f4b
Dharm-Singh-45 <dharmjat45@gmail.com>
2024-12-21 19:38:08 +0530

    index.js issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,6 @@ import { render } from 'preact'
 import './index.css'
 
 import { RouterProvider } from 'react-router-dom'
-import router from './routes/index.js'
+import router from './routes/index.jsx'
 
 render(<RouterProvider router={router} />, document.getElementById('app'))

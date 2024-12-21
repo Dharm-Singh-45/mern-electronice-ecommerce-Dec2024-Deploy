a724951ee7ba65b9393d624324dd1c248faf3a97
Dharm-Singh-45 <dharmjat45@gmail.com>
2024-12-21 22:12:07 +0530

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,13 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "https://mern-electronice-ecommerce-frontend.vercel.app",
-    credentials:true
-}))
+    origin: process.env.FRONTEND_URL || "https://mern-electronice-ecommerce-frontend.vercel.app", // Allows only the frontend's URL
+    credentials: true, // Allows cookies to be sent with requests
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+}));
+
+
 app.use(express.json())
 app.use(cookieParser())
 // routes 

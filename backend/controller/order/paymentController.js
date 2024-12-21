240858107ea00bf3e96f740063d7e05d48b7b53a
Dharm-Singh-45 <dharmjat45@gmail.com>
2024-12-21 20:49:41 +0530

    success page issue

--- backend/controller/order/paymentController.js
+++ backend/controller/order/paymentController.js
@@ -41,7 +41,7 @@ const paymentController = async (req, res) => {
           quantity : item.quantity
         };
       }),
-      success_url : `${process.env.FRONTEND_URL}/success`,
+      success_url : `${process.env.FRONTEND_URL || "https://mern-electronice-ecommerce-dec2024-5id3.onrender.com"}/success`,
       cancel_url : `${process.env.FRONTEND_URL}/cancel`
     };
 

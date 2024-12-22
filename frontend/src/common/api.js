c735ca093a840a793fd3bbd116f0b6821343137f
Dharm-Singh-45 <dharmjat45@gmail.com>
2024-12-22 18:20:23 +0530

    cors issue

--- frontend/src/common/api.js
+++ frontend/src/common/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mern-electronice-ecommerce-backend.vercel.app/api",
+  baseURL: "https://mern-electronice-ecommerce-dec2024.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
 
